16e2813cbfe380b6ef1dde40e614ae91d870d687
dfpj <pysedev@gmail.com>
2023-01-23 13:12:23 +0100

    ready

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from djnago.http import HttpResponse
+from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("hello from server")
