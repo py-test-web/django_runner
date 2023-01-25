cb1fde82f8ac4ebdc605688c55ff20f86e006043
dfpj <pysedev@gmail.com>
2023-01-25 20:46:28 +0100

    ready to deploy

--- deploy.sh
+++ deploy.sh
@@ -100,9 +100,10 @@ http {
       listen 80;
 
       location /static/ {
+        autoindex off;
         alias /var/www/static/;
       }
-      
+
       location / {
           proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
           proxy_set_header Host \$host;
